fec69667c81851305da564bf7c71758a9291ca70
Arisekola <shuaibazeez14@gmail.com>
2024-08-10 19:25:20 +0100

    more changes

--- src/app/components/GotoWhatsapp.tsx
+++ src/app/components/GotoWhatsapp.tsx
@@ -1,22 +1,43 @@
+'use client'
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import whatsapp from '../../../public/images/download.png';
 
 const GotoWhatsapp = () => {
-  const whatsappNumber = "08064272889";
+  const [isVisible, setIsVisible] = useState(false);
+  const whatsappNumber = "+2348074194566";
   const whatsappLink = `https://wa.me/${whatsappNumber}`;
 
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
   return (
-    <div className="fixed bottom-5 left-4 ">
-      <div className="flex justify-between cursor-pointer items-center">
-        <Link href={whatsappLink} target="_blank">
-          <Image src={whatsapp} width={40} height={40} alt="whatsapp" />
-        </Link>
-        <p className="bg-Dark-Violet text-white py-2 px-4 rounded-md animate-pulse">
-          Chat with us?
-        </p>
+    isVisible && (
+      <div className="fixed bottom-5 left-4">
+        <div className="flex justify-between items-center cursor-pointer">
+          <Link href={whatsappLink} target="_blank">
+            <Image src={whatsapp} width={40} height={40} alt="whatsapp" />
+          </Link>
+          <p className="bg-Dark-Violet text-white py-2 px-4 rounded-md animate-pulse">
+            Chat with us?
+          </p>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
--- src/app/components/Homebanner.tsx
+++ src/app/components/Homebanner.tsx
@@ -1,10 +1,12 @@
+'use client'
 import Image from 'next/image';
 import pattern from '../../../public/images/bg-pattern-intro-right-desktop.svg';
 import pattern2 from '../../../public/images/bg-pattern-intro-left-desktop.svg';
 import desktop from '../../../public/images/desktop.png';
-import Link from 'next/link';
+
 
 const Homebanner = () => {
+
   return (
     <main className="bg-Dark-Violet min-h-screen text-white flex xs:py-16 md:py-0 md:items-center relative">
     <div className='absolute top-5 z-50 right-0 xs:w-[150px] md:w-[300px] lg:w-[500px]'>
@@ -17,13 +19,14 @@ const Homebanner = () => {
     <div className='w-10/12 mx-auto flex xs:flex-col items-center md:flex-row gap-10'>
       <div className='flex w-full md:w-[600px] flex-col gap-6'>
       <hr className='w-20 border-b-[2px] border-red-500' />
-        <h1 className='xl:text-5xl lg:text-3xl md:text-xl xs:text-2xl font-semibold '> Transforming Supermarkets into <br /> Efficient  Profitable Enterprises</h1>
+        <h1 className='xl:text-5xl lg:text-3xl md:text-xl xs:text-2xl font-semibold '>Transforming Supermarkets into <br /> Efficient  Profitable Enterprises</h1>
         <p className='lg:text-lg xs:text-sm font-medium leading-6'>
          Denodata Business Solutions is a retail consulting firm aimed at offering top class retail service solutions and consultancy to enable new and growing retail businesses achieve their retail objectives thereby boosting profitability and safe guarding company assets. This is achieved through efficient business processes, recruitment, staff training, general inventory management procurement guidelines and recommendations.
         </p>
-        <div className=" bg-red-500 hover:bg-red-950 w-32 rounded-md py-2">
-              <Link className="px-2 mx-auto flex items-center justify-center text-sm" href='/about'>Read More</Link> 
-          </div>
+        {/* <div onClick={handleReadMoreClick} className=" bg-red-500 hover:bg-red-950 w-32 rounded-md py-2">
+              <button className="px-2 mx-auto flex items-center justify-center cursor-pointer text-sm">Read More</button> 
+          </div> */}
+        
       </div>
       
           <div className='xl:w-[500px] md:w-[600px] w-full xs:mt-10 md:mt-0 xl:mt-10  '>
@@ -36,6 +39,7 @@ const Homebanner = () => {
 }
 
 export default Homebanner
+
 
 
 
